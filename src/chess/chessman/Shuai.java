fd8204c99300b68c476c1fb2ecd5ddb813d1fdf2
gjz14 <544402104@qq.com>
2020-04-29 17:41:26 -0700

    add face king logic in Shuai's move locations

--- src/chess/chessman/Shuai.java
+++ src/chess/chessman/Shuai.java
@@ -20,6 +20,15 @@ public class Shuai extends Piece {
             return "img/rb.png";
     }
 
+    public boolean checkFaceKing(int x, int y, Board board){
+        int delta_x = (color==0)?-1:1;
+        int dx = x+delta_x,dy = y;
+        while(Common.isInBoard(dx, dy) && board.getColor(dx, dy) == -1 )
+            dx += delta_x;
+        if(Common.isInBoard(dx, dy) && board.pieces.get(Common.encoder(dx, dy)).getClass()==Shuai.class)
+            return true;
+        return false;
+    }
 
     public List<Integer> getMoveLocations(int x, int y, Board board) {
         List<Integer> locs = new ArrayList<>();
@@ -27,7 +36,7 @@ public class Shuai extends Piece {
         for(int i=0;i<4;i++){
             int delta_x = dirs[i], delta_y = dirs[i+1];
             int dx = x+delta_x, dy = y+delta_y;
-            if(Common.isInPalace(dx, dy, color) && board.getColor(dx, dy)!=color)
+            if(Common.isInPalace(dx, dy, color) && board.getColor(dx, dy)!=color &&!checkFaceKing(dx, dy, board))
                 locs.add(Common.encoder(dx, dy));
         }
         return locs;
